552ab428338c44b400cb94e26bf6b1b4834727b6
邢尔军 <xingej@163.com>
2017-11-07 19:45:20 +0800

    添加了单例模式之饿汉式
    最好使用懒汉式，因为饿汉式，不管你用不用都给你加载，很有可能造成浪费

---
+++ xingej-mode/src/main/java/com/xingej/patterns/singleton/example1/SingleDemo1.java
@@ -0,0 +1,24 @@
+package com.xingej.patterns.singleton.example1;
+
+/**
+ * 单例模式：饿汉式
+ * 
+ * @author erjun 2017年11月7日 下午7:38:40
+ */
+public class SingleDemo1 {
+
+    // static 变量会在类装载时初始化，不会涉及到多线程问题。
+    // 虚拟机保证只会装载一次，肯定不会发生并发访问的问题。
+    // 因此，可以省略synchronized关键字的
+    private static SingleDemo1 instance = new SingleDemo1();
+
+    private SingleDemo1() {
+
+    }
+
+    // 问题：如果只是加载了本类，而没有调用getInstance()，甚至永远没有调用，则会造成资源浪费
+    public static SingleDemo1 getInstance() {
+        return instance;
+    }
+
+}
