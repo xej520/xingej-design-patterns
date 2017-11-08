1d41792558add346ef28437709cb47c663fc5d72
邢尔军 <xingej@163.com>
2017-11-08 20:07:51 +0800

    添加案例：使用单例模式读取配置文件，并添加测试用例

---
+++ xingej-mode/src/main/java/com/xingej/patterns/singleton/example6/SingleMode.java
@@ -0,0 +1,54 @@
+package com.xingej.patterns.singleton.example6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SingleMode {
+
+    private SingleMode() {
+        // 创建对象时，会默认加载配置文件
+        readConfig();
+    }
+
+    private static class InnerSingle {
+        private static SingleMode instance = new SingleMode();
+    }
+
+    public static SingleMode getInstance() {
+        return InnerSingle.instance;
+    }
+
+    // ---------------------上面是单例模式----------------------------
+    private Properties properties;
+
+    private InputStream inputStream = null;
+
+    private void readConfig() {
+        properties = new Properties();
+
+        try {
+            // 注意，配置文件的路径
+            inputStream = SingleMode.class.getResourceAsStream("/single.properties");
+
+            properties.load(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != inputStream) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 提供对外的接口，
+    // 查询配置文件的属性信息
+    public String getProperties(String key) {
+        return properties.getProperty(key);
+    }
+}
---
+++ xingej-mode/src/test/java/com/xingej/patterns/singleton/example6/SingleModeTest.java
@@ -0,0 +1,17 @@
+package com.xingej.patterns.singleton.example6;
+
+import org.junit.Test;
+
+public class SingleModeTest {
+
+    // 通过单例方式，来读取配置文件
+    @Test
+    public void testReadConfigBySingleMothod() {
+        SingleMode instance = SingleMode.getInstance();
+
+        String name = instance.getProperties("name");
+
+        System.out.println("---name:\t" + name);
+    }
+
+}
