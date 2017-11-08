007137942612da4770c3c824c1ef2e6f01dd82b5
邢尔军 <xingej@163.com>
2017-11-08 11:47:27 +0800

    静态内部类方式创建单例模式
    优点是：高并发(不用使用同步synchronized关键字)，延迟加载

---
+++ xingej-mode/src/main/java/com/xingej/patterns/singleton/example4/SingletonDemo01.java
@@ -0,0 +1,26 @@
+package com.xingej.patterns.singleton.example4;
+
+/**
+ * 静态内部类方式创建单例模式
+ * 
+ * 第一、当你初始化SingletonDemo01的时候，并不会立即加载静态内部类的。 外部类没有static属性，则不会像饿汉式那样立即加载对象。
+ * 第二、只有真正调用getiInstance()，才会加载静态内部类。 第三、 加载类时是线程安全的， instance的类型是static final，
+ * 从而保证了内存中只有这样一个实例存在，而且只能被赋值一次， 从而保证了线程安全性 具有高并发和延迟加载的优点
+ * 
+ * @author erjun 2017年11月8日 上午11:38:31
+ */
+public class SingletonDemo01 {
+    // 静态内部类，并不会立即加载的，只有调用时才加载的
+    private static class SingletonClassInstance {
+        private static final SingletonDemo01 instance = new SingletonDemo01();
+    }
+
+    private SingletonDemo01() {
+
+    }
+
+    public static SingletonDemo01 getInstance() {
+        return SingletonClassInstance.instance;
+    }
+
+}
