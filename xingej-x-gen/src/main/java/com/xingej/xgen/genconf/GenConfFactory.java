ecd09887b11b7827af159fc54e2731a360f30031
邢尔军 <xingej@163.com>
2017-11-25 21:35:54 +0800

    加入简单工厂模式；GenConfFactory；存在问题：浪费时间，浪费内存空间

---
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/GenConfFactory.java
@@ -0,0 +1,21 @@
+package com.xingej.xgen.genconf;
+
+/**
+ * 简单工厂的本质是： 选择实现
+ * 
+ * @author erjun 2017年11月25日 下午9:27:35
+ */
+public class GenConfFactory {
+
+    // 禁止 外界创建此对象
+    private GenConfFactory() {
+    }
+
+    // 存在的问题？
+    // 如果此对象，被创建多次的话，那么就会重复获取配置数据，浪费程序运行时间，
+    // 以及浪费内存空间
+    public static GenConfEbi createGenConfEbi() {
+
+        return new GenConfEbo();
+    }
+}
