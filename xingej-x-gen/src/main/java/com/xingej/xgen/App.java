8f1f59780f0777eeafd7c2ebfb485415a58b0445
邢尔军 <xingej@163.com>
2017-11-25 18:11:20 +0800

    构建x-gen项目工程；目的：综合练习GOF23设计模式

---
+++ xingej-x-gen/src/main/java/com/xingej/xgen/App.java
@@ -0,0 +1,13 @@
+package com.xingej.xgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
