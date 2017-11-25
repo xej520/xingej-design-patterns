cbbd6beb9cba58a4a3fa1587d6b6a8d35d85051c
邢尔军 <xingej@163.com>
2017-11-25 19:43:56 +0800

    实现GenConfEbi 业务实现接口，注意，仅仅是打起了框架，并未真正的实现API啊

---
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/GenConfEbo.java
@@ -0,0 +1,24 @@
+package com.xingej.xgen.genconf;
+
+import java.util.Map;
+
+import com.xingej.xgen.genconf.vo.GenConfModel;
+import com.xingej.xgen.genconf.vo.ModuleConfModel;
+
+//业务实现对象
+public class GenConfEbo implements GenConfEbi {
+
+    @Override
+    public GenConfModel getGenConf() {
+        // 获取相应的配置数据
+        return null;
+    }
+
+    @Override
+    public Map<String, ModuleConfModel> getMapModuleConf() {
+
+        // 获取相应的配置数据
+        return null;
+    }
+
+}
