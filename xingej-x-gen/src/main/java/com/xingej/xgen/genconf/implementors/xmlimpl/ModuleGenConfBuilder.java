22f6f0a7340fd2a60f82f3cf45c6b3246d2d6bf7
邢尔军 <xingej@163.com>
2017-12-07 20:17:05 +0800

    完成ThemeBuilder 和 ModuleGenConfBuilder 的编写

--- xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ModuleGenConfBuilder.java
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ModuleGenConfBuilder.java
@@ -1,5 +1,71 @@
 package com.xingej.xgen.genconf.implementors.xmlimpl;
 
-public class ModuleGenConfBuilder {
+import com.xingej.xgen.genconf.constants.ModuleGenConfEnum;
+
+public class ModuleGenConfBuilder extends CommonBuilder<ModuleGenConfBuilder> {
+
+    // 用来记录分步骤，拼接的字符串，也就是最终的产品
+    private StringBuffer buffer = new StringBuffer();
+
+    public ModuleGenConfBuilder addModuleGenConf() {
+        buffer.append(ModuleGenConfEnum.ModuleGenConf);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addNeedGenTypes() {
+        buffer.append(ModuleGenConfEnum.NeedGenTypes);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addNeedGenType() {
+        buffer.append(ModuleGenConfEnum.NeedGenType);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addExtendConfs() {
+        buffer.append(ModuleGenConfEnum.ExtendConfs);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addExtendConf() {
+        buffer.append(ModuleGenConfEnum.ExtendConf);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addId() {
+        buffer.append(ModuleGenConfEnum.id);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addIsSingle() {
+        buffer.append(ModuleGenConfEnum.isSingle);
+
+        return this;
+    }
+
+    public ModuleGenConfBuilder addNeedGenOutType() {
+        buffer.append(ModuleGenConfEnum.NeedGenOutType);
+
+        return this;
+    }
+
+    @Override
+    protected StringBuffer getBuilderBuffer() {
+
+        return this.buffer;
+    }
+
+    @Override
+    protected ModuleGenConfBuilder getBuilderClassInstance() {
+
+        // 调用自己
+        return this;
+    }
 
 }
--- xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ThemeBuilder.java
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ThemeBuilder.java
@@ -1,5 +1,87 @@
 package com.xingej.xgen.genconf.implementors.xmlimpl;
 
-public class ThemeBuilder {
+import com.xingej.xgen.genconf.constants.ThemeEnum;
 
+public class ThemeBuilder extends CommonBuilder<ThemeBuilder> {
+    // 用来记录分步骤，拼接的字符串，也就是最终的产品
+    private StringBuffer buffer = new StringBuffer();
+
+    public ThemeBuilder addTheme() {
+        buffer.append(ThemeEnum.Theme);
+
+        return this;
+    }
+
+    public ThemeBuilder addGenOutType() {
+        buffer.append(ThemeEnum.GenOutType);
+
+        return this;
+    }
+
+    public ThemeBuilder addGenOutTypes() {
+        buffer.append(ThemeEnum.GenOutTypes);
+
+        return this;
+    }
+
+    public ThemeBuilder addGenType() {
+        buffer.append(ThemeEnum.GenType);
+
+        return this;
+    }
+
+    public ThemeBuilder addGenTypes() {
+        buffer.append(ThemeEnum.GenTypes);
+
+        return this;
+    }
+
+    public ThemeBuilder addId() {
+        buffer.append(ThemeEnum.id);
+
+        return this;
+    }
+
+    public ThemeBuilder addParam() {
+        buffer.append(ThemeEnum.Param);
+
+        return this;
+    }
+
+    public ThemeBuilder addParams() {
+        buffer.append(ThemeEnum.Params);
+
+        return this;
+    }
+
+    public ThemeBuilder addProvider() {
+        buffer.append(ThemeEnum.Provider);
+
+        return this;
+    }
+
+    public ThemeBuilder addProviders() {
+        buffer.append(ThemeEnum.Providers);
+
+        return this;
+    }
+
+    public ThemeBuilder addType() {
+        buffer.append(ThemeEnum.type);
+
+        return this;
+    }
+
+    @Override
+    protected StringBuffer getBuilderBuffer() {
+
+        return this.buffer;
+    }
+
+    @Override
+    protected ThemeBuilder getBuilderClassInstance() {
+
+        // 调用自己
+        return this;
+    }
 }
