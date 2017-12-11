4536dae69cb19cf48e2b8557ea89c0d7735abf74
邢尔军 <xingej@163.com>
2017-12-12 04:53:36 +0800

    完成ModuleGenConfXmlImpl类中getBaseModuleConfModel 的编写

--- xingej-x-gen/src/main/java/com/xingej/xgen/genconf/constants/ModuleGenConfEnum.java
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/constants/ModuleGenConfEnum.java
@@ -1,5 +1,10 @@
 package com.xingej.xgen.genconf.constants;
 
+/**
+ * 定义常量
+ * 
+ * @author erjun 2017年12月11日 上午7:11:04
+ */
 public enum ModuleGenConfEnum {
-    ModuleGenConf, NeedGenTypes, NeedGenType, id, NeedGenOutType, ExtendConfs, ExtendConf, isSingle;
+    ModuleGenConf, NeedGenTypes, NeedGenType, id, NeedGenOutType, ExtendConfs, ExtendConf, isSingle, fileName;
 }
--- xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ModuleGenConfXmlImpl.java
+++ xingej-x-gen/src/main/java/com/xingej/xgen/genconf/implementors/xmlimpl/ModuleGenConfXmlImpl.java
@@ -3,9 +3,13 @@ package com.xingej.xgen.genconf.implementors.xmlimpl;
 import java.util.List;
 import java.util.Map;
 
+import com.xingej.xgen.genconf.constants.ModuleGenConfEnum;
 import com.xingej.xgen.genconf.implementors.ModuleGenConfImplementor;
 import com.xingej.xgen.genconf.vo.ExtendConfModel;
 import com.xingej.xgen.genconf.vo.ModuleConfModel;
+import com.xingej.xgen.util.readxml.Context;
+import com.xingej.xgen.util.readxml.Parser;
+import com.xingej.xgen.util.readxml.ReadXmlExpression;
 
 /**
  * 
@@ -15,7 +19,12 @@ public class ModuleGenConfXmlImpl implements ModuleGenConfImplementor {
 
     @Override
     public ModuleConfModel getBaseModuleConfModel(Map<String, String> param) {
-        return null;
+        ModuleConfModel mcm = new ModuleConfModel();
+
+        // 设置模块的标识
+        this.parseModuleId(mcm, this.getContext(param));
+
+        return mcm;
     }
 
     @Override
@@ -26,6 +35,40 @@ public class ModuleGenConfXmlImpl implements ModuleGenConfImplementor {
     @Override
     public Map<String, ExtendConfModel> getMapExtends(Map<String, String> param) {
         return null;
+    }
+
+    ////// ---------------------Context---------------------------
+    private Context getContext(Map<String, String> param) {
+        Context context = null;
+
+        try {
+            // 下面的形式，还是自己去拼接的
+            // context = Context.getInstance(ExpressionEnum.xmlFilePre.getExpr()
+            // + GenConfEnum.GenConf
+            // + ExpressionEnum.dot.getExpr() + ExpressionEnum.xml);
+
+            // 下面使用builder进行拼接
+            // 也就是buildder模式
+            context = Context.getInstance(
+                    new GenConfBuilder().addXmlFilePre().addOtherValue(param.get(ModuleGenConfEnum.fileName)).build());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return context;
+    }
+
+    ////// ---------------------BaseModuleConfModel---------------------------
+    // 这是按引用传递，私有方法里改变了mcm，其他地方，相应改变。
+    // 按引用传递，可以不要返回值的
+    private void parseModuleId(ModuleConfModel mcm, Context context) {
+        context.init();
+        ReadXmlExpression re = Parser.parse(new ModuleGenConfBuilder().addModuleGenConf().addDot().addId().build());
+        String[] ss = re.interpret(context);
+
+        mcm.setModuleId(ss[0]);
+
     }
 
 }
