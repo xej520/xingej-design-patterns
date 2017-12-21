61421cfb728bec01b3acc89e6ae7fab2caa332b5
邢尔军 <xingej@163.com>
2017-12-21 11:15:51 +0800

    【CSDN】 ---> 工厂方法模式；使用"反射+XML"文件的方式，进行扩展；不需要修改源代码；使用什么日志文件，只需要在配置文件里，添加就可以了

--- xingej-dp-csdn/src/test/java/com/xingej/csdn/createtype/factory/factoryMethodPatten/FactoryMethodPatternTest.java
+++ xingej-dp-csdn/src/test/java/com/xingej/csdn/createtype/factory/factoryMethodPatten/FactoryMethodPatternTest.java
@@ -2,6 +2,8 @@ package com.xingej.csdn.createtype.factory.factoryMethodPatten;
 
 import org.junit.Test;
 
+import com.xingej.csdn.utils.XMLUtils;
+
 /**
  * 
  * @author erjun 2017年12月21日 上午10:25:58
@@ -22,6 +24,32 @@ public class FactoryMethodPatternTest {
         Logger logger = new DatabaseLoggerFactory().getInstance();
 
         System.out.println("----logger-----:\t");
+
+        logger.show();
+    }
+
+    // -----反射+XML方式，进行扩展
+    // 其实，不一定是XML文件，也可用是普通的properties文件
+    // 只是，要写专门的解析器去解释配置文件而已
+    @Test
+    public void test3() {
+        // 1、获取配置文件中的工厂名称
+        String factoryClassName = XMLUtils.getClazz();
+
+        // 2、通过反射来创建工厂实例
+        LoggerFactory loggerFactory = null;
+
+        try {
+            Class clazz = Class.forName("com.xingej.csdn.createtype.factory.factoryMethodPatten." + factoryClassName);
+
+            loggerFactory = (LoggerFactory) clazz.newInstance();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 3、通过工厂实例，来创建日志对象
+        Logger logger = loggerFactory.getInstance();
 
         logger.show();
     }
